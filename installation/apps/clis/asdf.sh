c394295ae97d2768267ff59f8af33ee4874c91e4
benedict <benedictong42@gmail.com>
2022-05-13 21:53:14 +1000

    chore: execute prettily

--- installation/apps/clis/asdf.sh
+++ installation/apps/clis/asdf.sh
@@ -4,16 +4,16 @@ install_asdf() {
   brew_install "asdf" "asdf"
 
   # install node
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-  asdf install nodejs 18.1.0
+  execute "asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git"
+  execute "asdf install nodejs 18.1.0"
  
   # install java
-  asdf plugin-add java https://github.com/halcyon/asdf-java.git
-  asdf install java zulu-11.54.23
+  execute "asdf plugin-add java https://github.com/halcyon/asdf-java.git"
+  execute "asdf install java zulu-11.54.23"
 
   # install ruby
-  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  execute "asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git"
 
   # install golang
-  asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+  execute "asdf plugin-add golang https://github.com/kennyp/asdf-golang.git"
 }
