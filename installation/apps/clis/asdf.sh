e033676a3f5bad5242d1043ac35ff0627209b39a
benedict <benedictong42@gmail.com>
2022-05-13 21:04:28 +1000

    chore: add java, ruby and golang

--- installation/apps/clis/asdf.sh
+++ installation/apps/clis/asdf.sh
@@ -6,4 +6,14 @@ install_asdf() {
   # install node
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
   asdf install nodejs 18.1.0
+ 
+  # install java
+  asdf plugin-add java https://github.com/halcyon/asdf-java.git
+  asdf install java zulu-11.54.23
+
+  # install ruby
+  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+  # install golang
+  asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 }
