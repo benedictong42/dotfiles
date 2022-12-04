6cc054175939e300b129c775e3e1f87a69af3590
benedict <benedictong42@gmail.com>
2022-12-03 21:49:27 -0800

    chore: add go

--- installation/apps/essentials/asdf.sh
+++ installation/apps/essentials/asdf.sh
@@ -13,6 +13,10 @@ install_asdf() {
   execute "asdf install nodejs 18.1.0"
   execute "asdf global nodejs 18.1.0"
  
+  # install g
+  execute "asdf plugin-add golang https://github.com/kennyp/asdf-golang.git"
+  execute "asdf install golang 1.19.3"
+  execute "asdf global golang 1.19.3"
   # install java
   execute "asdf plugin-add java https://github.com/halcyon/asdf-java.git"
   execute "asdf install java zulu-11.54.23"
